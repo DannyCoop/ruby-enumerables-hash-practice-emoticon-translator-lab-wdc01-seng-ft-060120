7026bdc41d05add38abb85c477e79602d7310d99
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-06-01 03:24:08 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,6 +21,7 @@ end
 
 def get_japanese_emoticon(file_path, emot)
   # code goes here
+<<<<<<< HEAD
   result = ""
   wrong_thing = "Sorry, that emoticon was not found"
   
@@ -36,11 +37,35 @@ def get_japanese_emoticon(file_path, emot)
     end
     result
    #binding.pry
+=======
+  wanted_emot = ""
+  sorry = "wanted_emot = value[:japanese]"
+    load_library('./lib/emoticons.yml').each do |key, value|
+     if emot == value[:english]
+       wanted_emot = value[:japanese]
+     end
+  
+    #binding.pry
+    end
+    wanted_emot
+    #binding.pry
+>>>>>>> 2f3850dda38531096cae92f863c8ba2577644472
 end
   
 
 
+<<<<<<< HEAD
 def get_english_meaning(file_path, emot)
+=======
+#get_japanese_emoticon()
+  
+  
+  
+  
+
+
+def get_english_meaning
+>>>>>>> 2f3850dda38531096cae92f863c8ba2577644472
   # code goes here
   result = ""
   wrong_thing = "Sorry, that emoticon was not found"
